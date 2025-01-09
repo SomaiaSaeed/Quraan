82e17ccb008a474a6181529175652a18318de8f6
Menshawy92 <madel5848@gmail.com>
2025-01-09 14:54:44 +0200

    listen page done

--- src/app/dashboard/listen/components/form/form.component.ts
+++ src/app/dashboard/listen/components/form/form.component.ts
@@ -42,24 +42,25 @@ export class FormComponent implements OnInit {
 	selectedAyaIds: number[] = [];
 	uniqueJozNumbersList: number[] = [];
 	hezbList: number[] = [];
-	rubList:string[] = [];
-	pagesList:number[] = []
+	rubList: string[] = [];
+	pagesList: number[] = [];
+	ayatListOfPages: number[] = []
 
 	constructor(private fb: FormBuilder, private _listenService: ListenService) {
 		// إنشاء النموذج باستخدام FormBuilder
 		this.form = this.fb.group({
-			suraFrom: ['',Validators],
-			ayaFrom: ['',Validators],
-			suraTo: ['',Validators],
-			ayaTo: ['',Validators],
-			JozFrom: ['',Validators],
-			JozTo: ['',Validators],
-			hezbFrom: ['',Validators],
-			hezbTo: ['',Validators],
-			rubFrom:['',Validators],
-			rubTo:['',Validators],
-			pageFrom:['',Validators],
-			pageTo:['', Validators],
+			suraFrom: ['', Validators.required],
+			ayaFrom: ['', Validators.required],
+			suraTo: ['', Validators.required],
+			ayaTo: ['', Validators.required],
+			JozFrom: ['', Validators.required],
+			JozTo: ['', Validators.required],
+			hezbFrom: ['', Validators.required],
+			hezbTo: ['', Validators.required],
+			rubFrom: ['', Validators.required],
+			rubTo: ['', Validators.required],
+			pageFrom: ['', Validators.required],
+			pageTo: ['', Validators.required],
 		});
 	}
 
@@ -72,10 +73,9 @@ export class FormComponent implements OnInit {
 	}
 
 
+	// تحديث الايات بناء على اختيار السور
 	updateAyaNumbers(type: 'from' | 'to') {
 		const selectedSura = this.form.get(`sura${type.charAt(0).toUpperCase() + type.slice(1)}`)?.value;
-
-		// console.log("selectedSura", selectedSura);
 
 		if (selectedSura) {
 			// إيجاد كل الآيات الخاصة بالسورة المختارة
@@ -84,19 +84,19 @@ export class FormComponent implements OnInit {
 
 			// استخراج أرقام الآيات الفعلية
 			const ayaNumbers = suraData.map(item => Number(item.Aya_N));
-			const ayaIds = suraData.map(item => Number(item.id));  // إضافة id لكل آية
+			const ayaIds = suraData.map(item => Number(item.id)); 
 
 			if (type === 'from') {
-				this.ayaNumbersFrom = ayaNumbers;  // لاستخدامها في الـ HTML
-				this.ayaIdsFrom = ayaIds;  // لاستخدامها داخلياً
-				this.form.get('ayaFrom')?.setValue('');  // إعادة تعيين القيمة
+				this.ayaNumbersFrom = ayaNumbers;
+				this.ayaIdsFrom = ayaIds; 
+				this.form.get('ayaFrom')?.setValue('');  
 			} else {
-				this.ayaNumbersTo = ayaNumbers;  // لاستخدامها في الـ HTML
-				this.ayaIdsTo = ayaIds;  // لاستخدامها داخلياً
+				this.ayaNumbersTo = ayaNumbers;
+				this.ayaIdsTo = ayaIds; 
 				this.form.get('ayaTo')?.setValue('');
 			}
-		} else {
-			// إذا لم يتم اختيار السورة
+		}
+		 else {
 			if (type === 'from') {
 				this.ayaNumbersFrom = [];
 				this.ayaIdsFrom = [];
@@ -110,7 +110,7 @@ export class FormComponent implements OnInit {
 		console.log("ayaIdsTo", this.ayaIdsTo);
 	}
 
-	// تحديث المصفوفة ملفات الصوت بعد اختيار عدد الايات من - إلى
+	// تحديث مصفوفة ملفات الصوت بعد اختيار عدد الايات من - إلى
 	generateAyaNumbers() {
 		const ayaFrom = Number(this.form.get('ayaFrom')?.value);
 		const ayaTo = Number(this.form.get('ayaTo')?.value);
@@ -122,23 +122,16 @@ export class FormComponent implements OnInit {
 		console.log("selectedSuraFrom:", selectedSuraFrom);
 		console.log("selectedSuraTo:", selectedSuraTo);
 
-		// جلب جميع الآيات مرتبة حسب الـ ID
+		//  جميع الآيات مرتبة حسب الـ ID
 		const allAyatSorted = this.searchInstance.table_othmani.sort((a, b) => Number(a.id) - Number(b.id));
 
-		// إيجاد الـ ID للآية البداية
 		const startAya = allAyatSorted.find(item =>
 			item.Sura_Name === selectedSuraFrom && Number(item.Aya_N) === ayaFrom
 		);
 
-		// إيجاد الـ ID للآية النهاية
 		const endAya = allAyatSorted.find(item =>
 			item.Sura_Name === selectedSuraTo && Number(item.Aya_N) === ayaTo
 		);
-
-		// if (!startAya || !endAya) {
-		// 	console.error("لم يتم العثور على الآيات المطلوبة.");
-		// 	return [];
-		// }
 
 		const startId = Number(startAya.id);
 		const endId = Number(endAya.id);
@@ -149,28 +142,25 @@ export class FormComponent implements OnInit {
 			return itemId >= startId && itemId <= endId;
 		});
 
-		// استخراج أرقام الآيات
 		const ayaNumbers = filteredAyat.map(item => Number(item.Aya_N));
 
 		console.log("Generated Aya Numbers:", ayaNumbers);
-		// return ayaNumbers;
 
 		if (ayaFrom && ayaTo) {
-			// this.selectedAyaNumbers = Array.from({ length: ayaTo - ayaFrom + 1 }, (_, i) => i + ayaFrom);
 			this.selectedAyaNumbers = ayaNumbers
-			// إنشاء مصفوفة Track تحتوي على { title, src }
+
 			this.audioFiles = this.selectedAyaNumbers.map(ayahNumber => {
-				// البحث عن نص الآية في table_othmani باستخدام Aya_N
-				const aya = this.searchInstance.table_othmani.find(item => item.Aya_N === String(ayahNumber)); // التأكد من أن Aya_N هو string
-				const title = aya ? aya.AyaText_Othmani : `Ayah ${ayahNumber}`; // استخدام AyaText_Othmani إذا وجد
+
+				const aya = this.searchInstance.table_othmani.find(item => item.Aya_N === String(ayahNumber)); 
+				const title = aya ? aya.AyaText_Othmani : `Ayah ${ayahNumber}`;
 
 				return {
-					title: title, // النص المستخرج
+					title: title, 
 					link: `https://cdn.islamic.network/quran/audio/64/ar.alafasy/${ayahNumber}.mp3`
 				};
 			});
 			this.generateJozNumbers()
-			console.log(this.audioFiles);  // تحقق من المصفوفة
+			console.log(this.audioFiles);
 		}
 	}
 
@@ -191,7 +181,6 @@ export class FormComponent implements OnInit {
 		if (startIndex !== -1 && endIndex !== -1 && startIndex <= endIndex) {
 			const ayatRange = this.searchInstance.table_othmani.slice(startIndex, endIndex + 1);
 
-			// استخراج القيم الفريدة للخاصية nOFJoz
 			const uniqueJozNumbers = Array.from(
 				new Set(ayatRange.map(item => item.nOFJoz))
 			);
@@ -212,22 +201,18 @@ export class FormComponent implements OnInit {
 		const selectedSuraFrom = this.form.get('suraFrom')?.value;
 		const selectedSuraTo = this.form.get('suraTo')?.value;
 
-		// جلب جميع الآيات بين الآية "من" والآية "إلى" مع مراعاة ترتيب السور
 		const filteredAyat = this.searchInstance.table_othmani.filter(item => {
 			const suraOrder = Number(item.nOFSura);
 			const ayaNumber = Number(item.nOFJoz);
 
-			// تحقق من الآيات داخل السورة الأولى
 			if (item.Sura_Name === selectedSuraFrom && ayaNumber >= JozFrom) {
 				return true;
 			}
 
-			// تحقق من الآيات داخل السورة الأخيرة
 			if (item.Sura_Name === selectedSuraTo && ayaNumber <= JozTo) {
 				return true;
 			}
 
-			// تحقق من الآيات بين السورتين (إذا كانوا مختلفين)
 			if (suraOrder > Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraFrom)?.nOFSura) &&
 				suraOrder < Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraTo)?.nOFSura)) {
 				return true;
@@ -236,7 +221,6 @@ export class FormComponent implements OnInit {
 			return false;
 		});
 
-		// استخراج الأحزاب الفريدة فقط في النطاق المحدد
 		const hezbNumbers = [...new Set(filteredAyat.map(item => Number(item.nOFHezb)))];
 
 		this.hezbList = hezbNumbers
@@ -254,22 +238,18 @@ export class FormComponent implements OnInit {
 		const selectedSuraFrom = this.form.get('suraFrom')?.value;
 		const selectedSuraTo = this.form.get('suraTo')?.value;
 
-		// جلب جميع الآيات بين الآية "من" والآية "إلى" مع مراعاة ترتيب السور
 		const filteredAyat = this.searchInstance.table_othmani.filter(item => {
 			const suraOrder = Number(item.nOFSura);
 			const hezbNumber = Number(item.nOFHezb);
 
-			// تحقق من الآيات داخل السورة الأولى
 			if (item.Sura_Name === selectedSuraFrom && hezbNumber >= hezbFrom) {
 				return true;
 			}
 
-			// تحقق من الآيات داخل السورة الأخيرة
 			if (item.Sura_Name === selectedSuraTo && hezbNumber <= hezbTo) {
 				return true;
 			}
 
-			// تحقق من الآيات بين السورتين (إذا كانوا مختلفين)
 			if (suraOrder > Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraFrom)?.nOFSura) &&
 				suraOrder < Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraTo)?.nOFSura)) {
 				return true;
@@ -278,7 +258,6 @@ export class FormComponent implements OnInit {
 			return false;
 		});
 
-		// استخراج الأحزاب الفريدة فقط في النطاق المحدد
 		const rubNumbers = [...new Set(filteredAyat.map(item => item.rub))];
 
 		this.rubList = rubNumbers
@@ -295,22 +274,18 @@ export class FormComponent implements OnInit {
 		const selectedSuraFrom = this.form.get('suraFrom')?.value;
 		const selectedSuraTo = this.form.get('suraTo')?.value;
 
-		// جلب جميع الآيات بين الآية "من" والآية "إلى" مع مراعاة ترتيب السور
 		const filteredAyat = this.searchInstance.table_othmani.filter(item => {
 			const suraOrder = Number(item.nOFSura);
 			const hezbNumber = Number(item.nOFHezb);
 
-			// تحقق من الآيات داخل السورة الأولى
 			if (item.Sura_Name === selectedSuraFrom && hezbNumber >= rubFrom) {
 				return true;
 			}
 
-			// تحقق من الآيات داخل السورة الأخيرة
 			if (item.Sura_Name === selectedSuraTo && hezbNumber <= rubTo) {
 				return true;
 			}
 
-			// تحقق من الآيات بين السورتين (إذا كانوا مختلفين)
 			if (suraOrder > Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraFrom)?.nOFSura) &&
 				suraOrder < Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraTo)?.nOFSura)) {
 				return true;
@@ -319,7 +294,6 @@ export class FormComponent implements OnInit {
 			return false;
 		});
 
-		// استخراج الأحزاب الفريدة فقط في النطاق المحدد
 		const pagesNumbers = [...new Set(filteredAyat.map(item => item.nOFPage))];
 
 		this.pagesList = pagesNumbers
@@ -328,58 +302,60 @@ export class FormComponent implements OnInit {
 		return pagesNumbers;
 	}
 
+	// عدد الأيات بعد اختيار عدد الصفحات 
+	getAyatOfOages() {
+		const pageFrom = Number(this.form.get('pageFrom')?.value);
+		const pageTo = Number(this.form.get('pageTo')?.value);
 
+		const selectedSuraFrom = this.form.get('suraFrom')?.value;
+		const selectedSuraTo = this.form.get('suraTo')?.value;
 
+		const filteredAyat = this.searchInstance.table_othmani.filter(item => {
+			const suraOrder = Number(item.nOFSura);
+			const pages = Number(item.nOFPage);
 
-	// if (ayaFrom && ayaTo) {
-	// 	this.selectedAyaNumbers = Array.from({ length: ayaTo - ayaFrom + 1 }, (_, i) => i + ayaFrom);
+			if (item.Sura_Name === selectedSuraFrom && pages >= pageFrom) {
+				return true;
+			}
 
-	// 	// إنشاء مصفوفة Track تحتوي على { title, src }
-	// 	this.audioFiles = this.selectedAyaNumbers.map(ayahNumber => {
-	// 		// البحث عن نص الآية في table_othmani باستخدام Aya_N
-	// 		const aya = this.searchInstance.table_othmani.find(item => item.Aya_N === String(ayahNumber)); // التأكد من أن Aya_N هو string
-	// 		const title = aya ? aya.AyaText_Othmani : `Ayah ${ayahNumber}`; // استخدام AyaText_Othmani إذا وجد
+			if (item.Sura_Name === selectedSuraTo && pages <= pageTo) {
+				return true;
+			}
 
-	// 		return {
-	// 			title: title, // النص المستخرج
-	// 			link: `https://cdn.islamic.network/quran/audio/64/ar.alafasy/${ayahNumber}.mp3`
-	// 		};
-	// 	});
+			if (suraOrder > Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraFrom)?.nOFSura) &&
+				suraOrder < Number(this.searchInstance.table_othmani.find(sura => sura.Sura_Name === selectedSuraTo)?.nOFSura)) {
+				return true;
+			}
 
-	// 	console.log(this.audioFiles);  // تحقق من المصفوفة
-	// }
+			return false;
+		});
 
+		const pagesNumbers = [...new Set(filteredAyat.map(item => item.Aya_N))];
 
+		this.ayatListOfPages = pagesNumbers;
 
+		console.log('الآيات الموجودة في الصفحات المختارة:', this.ayatListOfPages);
 
+		// الصوتيات
+		if (this.ayatListOfPages) {
+			this.audioFiles = this.ayatListOfPages.map(ayahNumber => {
+				// البحث عن نص الآية في table_othmani باستخدام Aya_N
+				const aya = this.searchInstance.table_othmani.find(item => item.Aya_N === String(ayahNumber));
+				const title = aya ? aya.AyaText_Othmani : `Ayah ${ayahNumber}`;
 
+				return {
+					title: title,
+					link: `https://cdn.islamic.network/quran/audio/64/ar.alafasy/${ayahNumber}.mp3`
+				};
+			});
+			console.log("this.audioFiles222222",this.audioFiles)
+		}
+	}
 
+	resetForm(){
+		this.form.reset();
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	updateToAya() { }
-	updateFromAya() { }
-	u() { }
-	onSubmit() { }
 }
 
 
