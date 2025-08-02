073f7404d9c79708bcf7a82d951b0cd48e7582ba
Somaia Saeed <ssmousa.c@stc.com.sa>
2025-08-02 09:19:59 +0300

    add motshabehat

--- src/app/core/constants/quraanImages.constant.ts
+++ src/app/core/constants/quraanImages.constant.ts
@@ -1,5 +1,73 @@
-export const IMAGES: any[] = [
-    { id: 1, src: 'assets/images/2/1.png', alt: 'Image 1' },
-    { id: 2, src: 'assets/images/2/2.png', alt: 'Image 2' },
-    { id: 3, src: 'assets/images/2/3.png', alt: 'Image 3' },
-  ];
+// export const IMAGES: any[] = [
+//     { id: 1, src: 'assets/images/2/1.png', alt: 'Image 1' },
+//     { id: 2, src: 'assets/images/2/2.png', alt: 'Image 2' },
+//     { id: 3, src: 'assets/images/2/3.png', alt: 'Image 3' },
+//   ];
+
+export const IMAGES: any[] = Array.from({ length: 10 }, (_, index) => { // length should be the total number of quran pages (6**)
+  const id = index + 1;
+  return {
+    id,
+    src: `assets/images/2/${id}.png`,
+    alt: `Image ${id}`,
+  };
+});
+
+export interface MotashabehatSpan {
+  isRight: boolean;
+  moade3: string;
+  height: string;
+  top: string;
+}
+
+export interface SpansOfColoredWords {
+  top: string;
+  left: string;
+  width: string;
+  color: string;
+  isStatic?: boolean;
+}
+
+export interface InputItem {
+  motashabehat: Motashabehat;
+  ayat: any[]; // Replace 'any' with the proper type if known
+  aya: string;
+  ayaId: string;
+  spans: Span[];
+  motashabehatSpans: MotashabehatSpan[];
+  spansOfColoredWords: SpansOfColoredWords[];
+  isActive: boolean;
+  href: string;
+  activeAya: number;
+  errorFactor: string;
+}
+
+interface Motashabehat {
+  isRight: boolean;
+  moade3: any[]; // Replace 'any' with the proper type if known
+  height: string;
+  top: string;
+}
+
+interface Span {
+  aya: string;
+  top: string;
+  left: string;
+  width: string;
+  height: string;
+}
+
+export interface MotashabehatSpan {
+  isRight: boolean;
+  moade3: string;
+  height: string;
+  top: string;
+}
+
+export interface SpansOfColoredWords {
+  top: string;
+  left: string;
+  width: string;
+  color: string;
+  isStatic?: boolean;
+}
