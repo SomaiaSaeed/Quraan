21e3da0737dfdca3e45dfdf3ae21892d5b247ee0
Somaia Saeed <ssmousa.c@stc.com.sa>
2026-04-16 13:07:33 +0200

    enhance footer: mobile bottom nav, remove desktop bar

--- src/app/dashboard/layout/components/footer/footer.component.ts
+++ src/app/dashboard/layout/components/footer/footer.component.ts
@@ -1,8 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { MatDialog, MatDialogRef } from "@angular/material/dialog";
-import { Router } from '@angular/router';
-import { PrintComponent } from 'src/app/shared/print/print.component';
-import { ReadersComponent } from 'src/app/shared/readers/readers.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -10,53 +6,7 @@ import { ReadersComponent } from 'src/app/shared/readers/readers.component';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  isOpenMenu: boolean = false;
-
-  constructor(public dialog: MatDialog,private router: Router) { }
-
-  ngOnInit() {
-  }
-
-  OpenMenu() {
-    this.isOpenMenu = !this.isOpenMenu
-  }
-
-  openReads(): void {
-    const dialogRef = this.dialog.open(ReadersComponent, {
-      width: "1000px",
-      panelClass: "popup-center",
-      data:this.isOpenMenu = false
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log("The dialog was closed");
-    });
-  }
-
-  openPrint(): void {
-    const dialogRef = this.dialog.open(PrintComponent, {
-      width: "600px",
-      panelClass: "popup-center",
-      data:this.isOpenMenu = false
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log("The dialog was closed");
-    });
-  }
-
-  openFavorite(){
-    this.router.navigate(['/favorite']);
-    this.isOpenMenu = false
-  }
-
-  openSimilarities(){
-    this.router.navigate(['/similarities']);
-    this.isOpenMenu = false
-  }
-
-  openAlsajadat(){
-    this.router.navigate(['/alsajadat']);
-    this.isOpenMenu = false
-  }
+  currentYear = new Date().getFullYear();
+  constructor() {}
+  ngOnInit() {}
 }
